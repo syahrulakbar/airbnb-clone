a688cb730a3a25044ef6c29dd7a476070c7d1b02
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2023-07-31 10:53:31 +0700

    style: adding tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
-import './globals.css'
+import { Nunito } from "next/font/google";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import "./globals.css";
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Trying make a clone of Airbnb",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       {/*
@@ -12,7 +19,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
-  )
+  );
 }
