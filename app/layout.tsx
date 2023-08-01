30ead83369b06af80f4090ef8bb5f8dc32875d4d
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2023-08-01 20:50:41 +0700

    style: add modal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { Nunito } from "next/font/google";
 
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
   title: "Airbnb",
@@ -21,6 +23,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       */}
       <head />
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
