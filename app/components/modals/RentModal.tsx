145600cc0e05ecf757f80d625d48c4c3b54b9ddf
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2023-08-03 21:01:00 +0700

    feat(couter comp): adding counter components rent

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,8 +10,8 @@ import CategoryInput from "../Inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 
 import CountrySelect from "../Inputs/CountrySelect";
-import Map from "../Map";
 import dynamic from "next/dynamic";
+import Counter from "../Inputs/Counter";
 
 enum STEPS {
   CATEGORY = 0,
@@ -49,6 +49,9 @@ export default function RentModal() {
 
   const category = watch("category");
   const location = watch("location");
+  const guestCount = watch("guestCount");
+  const roomCount = watch("roomCount");
+  const bathroomCount = watch("bathroomCount");
 
   const Map = useMemo(
     () =>
@@ -114,6 +117,35 @@ export default function RentModal() {
         <Heading title="Where is your place located?" subtitle="Help guest find you!" />
         <CountrySelect value={location} onChange={(value) => setCustomValue("location", value)} />
         <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="what amenities do you have?"
+        />
+        <Counter
+          title="Guest"
+          subtitle="How many guests do you allow?"
+          value={guestCount}
+          onChange={(value) => setCustomValue("guestCount", value)}
+        />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+          value={roomCount}
+          onChange={(value) => setCustomValue("roomCount", value)}
+        />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+          value={bathroomCount}
+          onChange={(value) => setCustomValue("bathroomCount", value)}
+        />
       </div>
     );
   }
